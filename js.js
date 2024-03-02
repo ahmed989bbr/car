0536849679cbe5790a1c4783ed75efd52e2f1f82
ahmed989bbr <107225916+ahmed989bbr@users.noreply.github.com>
2024-03-02 13:56:32 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,3 +1,37 @@
+// let root = document.querySelector('.card-group');
+
+async function s() {
+    const time = new Date()
+    const response = await fetch('./data.json');
+    const json = await response.json();
+    arr = [...json];
+    console.log(arr); // Log inside the async function when arr is populated
+
+    arr.forEach(el => {
+        let div = document.createElement('div');
+        div.innerHTML = `
+            
+                <div class="card">
+                    <img src="${el.image}" class="card-img-top" alt="${el.title}">
+                    <div class="card-body">
+                        <h5 class="card-title">${el.title}</h5>
+                        <p class="card-text">${el.class}</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated ${time.getFullYear()}</small>
+                    </div>
+                </div>
+          
+        `;
+        root.appendChild(div);
+    });
+}
+
+var arr;
+s();
+
+
+
 function loginFunction() {
     const loginU = document.querySelector('.Username');
     const password = document.querySelector('.Password');
@@ -17,4 +51,30 @@ function loginFunction() {
     } else {
         alert('Invalid username or password');
     }
+}
+
+
+
+const videos = document.querySelectorAll('video');
+const buttons = document.querySelectorAll('button');
+
+videos.forEach(video => {
+  video.addEventListener("mouseenter", () => {
+    video.play();
+    pauseOtherVideos(video);
+  });
+});
+
+videos.forEach(video => {
+  video.addEventListener("mouseleave", () => {
+    video.pause();
+    pauseOtherVideos(video);
+  });
+});
+function pauseOtherVideos(currentVideo) {
+  videos.forEach(video => {
+    if (video !== currentVideo) {
+      video.pause();
+    }
+  });
 }
